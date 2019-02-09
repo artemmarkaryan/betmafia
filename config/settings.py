417efb4e04332676e85d3368e393e3ce5d35d940
artemmarkaryan <artemandriy@gmail.com>
2019-02-09 16:03:06 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,8 @@
+import os
+
 token = '570570151:AAE-cbnktQk1lTrQy14Tu-MQRAPg785Ky8w'
 
-database_url = 'postgres://wfgssloebauhqa:a5bf0147b20d' \
-               '9c1db13887a64877f2bdeac58014ade5aa78a0' \
-               '76ad9816196b22@ec2-54-246-84-200.eu-we' \
-               'st-1.compute.amazonaws.com:5432/d4388l' \
-               '3nkf43tn'
+database_url = os.environ['DATABASE_URL']
 
 
 test_link = 'http://telegra.ph/fffgfg-05-09'
